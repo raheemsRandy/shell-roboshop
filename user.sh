a48e97dbb7315b204a6138f6d192f5de6020df73
raheemshaik <raheemshaik2404@gmail.com>
2025-06-29 23:59:05 +0530

    commit

--- user.sh
+++ user.sh
@@ -33,48 +33,49 @@ Validate(){
         exit 1
     fi
 }
+#&>>Log_file
 
 
-dnf module disable nodejs -y
-Validate $? "Copying mongodb repo"
+dnf module disable nodejs -y&>>Log_file
+Validate $? "disable nodejs"
 
-dnf module enable nodejs:20 -y
-
+dnf module enable nodejs:20 -y&>>Log_file
 dnf install nodejs -y
+Validate $? "enabling and installing"
 
 id roboshop
 if [ $? -eq 0 ]
 then    
     echo "user already exists"
 else
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop&>>Log_file
     Validate $? "Adding systemuser"
 fi
 
-mkdir -p /app 
+mkdir -p /app &>>Log_file
 Validate $? "Creating directory"
 
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip 
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>Log_file
 Validate $? "Downloading the content"
 
-rm -rf /app/*
+rm -rf /app/*&>>Log_file
 Validate $? "Removing the content in app folder"
 
-cd /app 
-unzip /tmp/user.zip
+cd /app &>>Log_file
+unzip /tmp/user.zip&>>Log_file
 Validate $? "extracting the content"
 
-cd /app 
-npm install 
+cd /app &>>Log_file
+npm install &>>Log_file
 Validate $? "installing npm"
 
 #vim /etc/systemd/system/user.service
-cp $Script_dir/user.service /etc/systemd/system/user.service
+cp $Script_dir/user.service /etc/systemd/system/user.service&>>Log_file
 Validate $? "copying service file"
 
 systemctl daemon-reload
 Validate $? "Daemon reload"
 
-systemctl enable user 
-systemctl start user
+systemctl enable user &>>Log_file
+systemctl start user&>>Log_file
 Validate $? "enable and start"
